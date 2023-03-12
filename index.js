f50e4e3819902c5533376f1232d815f707d88bda
ryanz12 <105404813+ryanz12@users.noreply.github.com>
2023-03-12 15:30:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-const express = require('express'); //import express library
-const app = express(); //instance of express
-
-//create new http server
+const express = require('express');
+const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
+    res.sendFile(__dirname + "/index.html");
+});
 
-io.on('connection', (socket) => {
-    io.emit('chat message', "A user connected");
-    socket.on('disconnect', ()=> {
-        io.emit('chat message', "A user has disconnected");
-    })
-})
-
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+io.on("connection", (socket) => {
+    //do something
+    
+    socket.on('chat message', (data) => {
+        io.emit('chat message', {name: data.name, message: data.message});
     });
-})
+    socket.on('disconnect', () => {
+        //do something
+    })
+});
+  
 
 server.listen(3000, () => {
-    console.log("Listening on port 3000");
+    console.log("Listening on port 3000*");
 })
+
